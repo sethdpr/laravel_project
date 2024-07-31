fd9c076214eb2259d9a90885101c408b34d6ca4b
Seth De Preter <seth.de.preter@student.ehb.be>
2024-07-31 17:52:53 +0200

    Collums added in migrations php file

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('geboortedatum')->nullable();
+            $table->string('nationaliteit')->nullable();
+            $table->boolean('is_admin')->default(false);    
             $table->rememberToken();
             $table->timestamps();
         });
