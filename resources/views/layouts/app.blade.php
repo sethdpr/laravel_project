537bcd65df2fcebbd8e07064c11dccb83c42e2a5
Seth De Preter <seth.de.preter@student.ehb.be>
2024-08-07 14:21:23 +0200

    Minor changes to nav bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,54 +3,39 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Manchester United') }}</title>
-
-    <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
-    <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
+    <style>
+        .navbar-brand.dropdown-toggle::after {
+    display: none;
+}
+
+    </style>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <div class="dropdown">
-                    <a class="navbar-brand dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="navbar-brand dropdown-toggle" href="#" id="appNameDropdown" data-bs-toggle="dropdown" aria-expanded="false">
                         Manchester United
                     </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <li><a class="dropdown-item" href="{{ url('/') }}">Home</a></li>
+                    <ul class="dropdown-menu" aria-labelledby="appNameDropdown">
+                        <li><a class="dropdown-item" href="{{ route('home') }}">Home</a></li>
                         <li><a class="dropdown-item" href="{{ route('squad') }}">Squad</a></li>
                         <li><a class="dropdown-item" href="{{ route('legends') }}">Legends</a></li>
                         <li><a class="dropdown-item" href="{{ route('calendar') }}">Calendar</a></li>
                     </ul>
                 </div>
-
-                @auth
-                    @if(Auth::user()->is_admin)
-                        <a href="{{ route('news.create') }}">New Post</a>
-                    @else
-                        <a>Dit is zichtbaar voor reguliere gebruikers.</a>
-                    @endif
-                @endauth
-
+                
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
@@ -59,7 +44,6 @@
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
-
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
@@ -70,17 +54,15 @@
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
-
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('profile', ['name' => Auth::user()->name]) }}">
                                         {{ __('Profile') }}
                                     </a>    
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
@@ -91,13 +73,9 @@
                 </div>
             </div>
         </nav>
-
         <main class="py-4">
             @yield('content')
         </main>
     </div>
-
-    <!-- Scripts -->
-    @vite(['resources/js/app.js'])
 </body>
 </html>
